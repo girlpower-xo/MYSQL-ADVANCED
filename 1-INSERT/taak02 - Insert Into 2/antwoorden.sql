8e17440523fadf0754617132516a3d6ba8fd3bfd
Iris Petersen <iris.petersen@student.rocva.nl>
2020-10-26 11:54:58 +0100

    done

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,6 @@
-
+INSERT INTO studenten (voornaam, achternaam, geboortedatum, woonplaats)
+VALUES ( 'Levu', 'ver', '2000-22-3' , 'amstelveen');
+INSERT INTO studenten (voornaam, achternaam, geboortedatum, woonplaats)
+VALUES ( 'eric', 'Petn', '2000-11-9' , 'amstelveen');
+INSERT INTO studenten (voornaam, achternaam, geboortedatum, woonplaats)
+VALUES ( 'ajas', 'pinda','2000-12-6' , 'Amsterdam');
