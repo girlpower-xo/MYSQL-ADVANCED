c9a12038252680bbdcb6d8d169fe9cbb4dc6198a
Iris Petersen <iris.petersen@student.rocva.nl>
2020-10-26 11:44:09 +0100

    done

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,5 @@
 
+INSERT INTO studenten (voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES ( 'Iris', 'Petersen', '9a', 'vigistraat', '5736 KD', '2000-22-3' , 'amstelveen');
+
+// en dan herhaal je dit met andere informatie//
