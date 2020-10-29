058f6d711d1628691af833e92fb89ee2b3d62027
Iris Petersen <iris.petersen@student.rocva.nl>
2020-10-29 11:31:41 +0100

    done

--- 2-UPDATE/taak02 - Update 2/antwoorden.sql
+++ 2-UPDATE/taak02 - Update 2/antwoorden.sql
@@ -1,8 +1,24 @@
 -- Opdracht 1
-
+UPDATE studenten2
+SET klas = '9X'
+WHERE klas = '9A' AND klas = '9A';
 -- Opdracht 2 
-
+UPDATE studenten2
+SET klas = '9Z'
+WHERE student_id >'7' AND student_id > '7';
 -- Opdracht 3
+UPDATE studenten2
+SET woonplaats = 'amsterdam'
+WHERE student_id <= '2' AND student_id <='2';
 
+UPDATE studenten2
+SET woonplaats = 'amsterdam'
+WHERE student_id > '7' AND student_id >'7';
 -- Opdracht 4
+UPDATE studenten2
+SET woonplaats = 'uithoorn'
+WHERE klas = '9X' AND klas = '9X';
 
+UPDATE studenten2
+SET woonplaats = 'uithoorn'
+WHERE woonplaats = 'amsterdam' AND woonplaats = 'amsterdam';
